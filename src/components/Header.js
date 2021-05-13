44cfa0f91038315250cda712769b8d4a6c450d51
citixenken <kmuyesu@gmail.com>
2021-05-13 09:25:33 +0300

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Container,
+  Row,
+  Button,
+} from "react-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Container>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+          <Navbar.Brand href="/">Pro-Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i> Login
+              </Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </header>
+  );
+}
+
+export default Header;
