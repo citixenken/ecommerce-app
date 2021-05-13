85f37c0801713864aa06fee49d1807913cf88112
citixenken <kmuyesu@gmail.com>
2021-05-13 20:15:39 +0300

    Implementing React Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,31 @@
 import React from "react";
-import {
-  Nav,
-  Navbar,
-  NavDropdown,
-  Container,
-  Row,
-  Button,
-} from "react-bootstrap";
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
-      <Container>
-        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-          <Navbar.Brand href="/">Pro-Shop</Navbar.Brand>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Pro-Shop</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart"></i> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-user"></i> Login
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Login
+                </Nav.Link>
+              </LinkContainer>
+
               <NavDropdown title="Profile" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Account</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -36,8 +39,8 @@ function Header() {
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
-        </Navbar>
-      </Container>
+        </Container>
+      </Navbar>
     </header>
   );
 }
